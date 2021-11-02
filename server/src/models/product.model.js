7a326680bc4fe9e3b66a6cb40949c23bda2f4ffe
rambhumukhia <email@masaischool.com>
2021-11-02 22:56:37 +0530

    schema and controller for product added

---
+++ server/src/models/product.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: Array, required: true },
+    size: { type: Array, required: true },
+    rating: { type: Number, required: true },
+    productDetails: { type: Array, required: true },
+    productCare: { type: Array, required: true },
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "category",
+      required: false,
+    },
+    subCategoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "subcategory",
+      required: false,
+    },
+    brandId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "brand",
+      required: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("product", productSchema);
+module.exports = Product;
