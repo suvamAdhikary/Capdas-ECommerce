0f5c251ec9b7861ac38310bfb7b6f69e1ea783e9
rambhumukhia <email@masaischool.com>
2021-11-03 22:06:54 +0530

    second push

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,3 @@ const app = express();
 app.use(express.json());
 
 module.exports = app;
-
-const productController = require("./controllers/product.controller");
-app.use("/products", productController);
