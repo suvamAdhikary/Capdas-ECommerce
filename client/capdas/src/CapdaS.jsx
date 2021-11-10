0f20fcaf68806ae8cae29013aedb24492d7e2bad
barkaleamol <amol_nj2_051@masai.school>
2021-11-11 00:52:12 +0530

    Payment page done

--- client/capdas/src/CapdaS.jsx
+++ client/capdas/src/CapdaS.jsx
@@ -5,14 +5,16 @@ import AddAddress from "./user/AddAddress"
 import { Bag } from "./BagPage/Bag";
 import Button from "./utils/Button"
 import HeaderNav from "../src/components/navbar/HeaderNav";
+import {Payment} from "../src/payment/Payment"
 export default function CapdaS() {
 
     return (<>
         {/* <Hp />
         <AddAddress /> */}
-        <Bag />
-        <Button>Checkout</Button>
-        {/* <HeaderNav/>
-        <Button>Next</Button>  */}
+        {/* <Bag />
+        <Button>Checkout</Button> 
+        <HeaderNav />*/}
+        <Payment>Payment</Payment>
+        <Button>Next</Button> 
     </>)
 }
