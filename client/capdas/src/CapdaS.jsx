ee9d950d197cbe9e6707b9f84c268d0fed68f2d0
Suvam Adhikary <adhikary.saheb408@gmail.com>
2021-11-11 11:54:58 +0530

    nav routing

--- client/capdas/src/CapdaS.jsx
+++ client/capdas/src/CapdaS.jsx
@@ -1,18 +1,28 @@
-// import HeaderNav from "./components/navbar/HeaderNav";
-// import FooterNav from "./components/navbar/FooterNav";
+import { Route, Routes } from "react-router-dom";
+import HeaderNav from "./components/navbar/HeaderNav";
+import FooterNav from "./components/navbar/FooterNav";
 
 import Signup from "./user/Signup"
-// import { Hp } from "./Homepage/Homepage"
+import { Hp } from "./Homepage/Homepage"
 import AddAddress from "./user/AddAddress"
 import Login from "./user/Login"
+import { useState } from "react"
 
 
 export default function CapdaS() {
+    const [showNav, setShowNav] = useState(true);
 
     return (<>
-        <Signup />
-        <Login />
-        <AddAddress />
+        { showNav ? <HeaderNav /> : null }
 
+        <Routes>
+
+            <Route path="/" exact element={<Hp />} />
+
+            
+
+        </Routes>
+
+        { showNav ? <FooterNav /> : null }
     </>)
 }
