d51446b9098c8534b66c5346476d77bf64d347ef
lokesh <lokesh15mca@gmail.com>
2021-11-13 14:38:19 +0530

    Adding search

--- client/capdas/src/Search/Search.jsx
+++ client/capdas/src/Search/Search.jsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react';
-import searchimg from './'
+import searchimg from '../Mend/img/search.png'
 import styles from './Search.module.css'
 function Search(){
 const [search, setSearch] = useState([]);
