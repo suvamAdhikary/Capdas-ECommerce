6f6b0e94b0f024711d4049e2d65d29e8e4226d90
rambhumukhia <email@masaischool.com>
2021-11-13 09:24:51 +0530

    filter static page without functionality

---
+++ client/capdas/src/Filter/Filter.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import styles from "./filter.module.css";
+
+const Filter = function () {
+  const [showFilterOption, setShowFilterOption] = useState(false);
+  const [showFilterOption1, setShowFilterOption1] = useState(false);
+  const [showFilterOption2, setShowFilterOption2] = useState(false);
+  return (
+    <>
+      <div className={styles.mainPageDiv}>
+        <div className={styles.FilterCategoryDiv}>
+          <div className={styles.bannerDiv}>
+            <div className={styles.bannerCrossDiv}>
+              <img
+                src="../assets/cross.svg"
+                alt="cross"
+                className={styles.bannerCrossimg}
+              />
+            </div>
+            <div className={styles.bannerCrossName}>Filters</div>
+          </div>
+          <div className={styles.firstFilterCat}>
+            <div
+              className={styles.cardInfoDiv}
+              onClick={() => {
+                setShowFilterOption(!showFilterOption);
+              }}
+            >
+              <div className={styles.filterName}>Brands</div>
+              <div className={styles.filterPlusSign}>+</div>
+            </div>
+            {showFilterOption ? (
+              <div>
+                <div className={styles.filterCategoryDiv}>Local Brands</div>
+                <div className={styles.filterCategoryDiv}>National Brands</div>
+              </div>
+            ) : null}
+          </div>
+
+          <div className={styles.firstFilterCat}>
+            <div
+              className={styles.cardInfoDiv}
+              onClick={() => {
+                setShowFilterOption1(!showFilterOption1);
+              }}
+            >
+              <div className={styles.filterName}>Price</div>
+              <div className={styles.filterPlusSign}>+</div>
+            </div>
+            {showFilterOption1 ? (
+              <div>
+                <div className={styles.filterCategoryDiv}>Less Than 1500</div>
+                <div className={styles.filterCategoryDiv}>Less Than 2500</div>
+                <div className={styles.filterCategoryDiv}>Above 2500</div>
+              </div>
+            ) : null}
+          </div>
+
+          <div className={styles.firstFilterCat}>
+            <div
+              className={styles.cardInfoDiv}
+              onClick={() => {
+                setShowFilterOption2(!showFilterOption2);
+              }}
+            >
+              <div className={styles.filterName}>Size</div>
+              <div className={styles.filterPlusSign}>+</div>
+            </div>
+            {showFilterOption2 ? (
+              <div>
+                <div className={styles.filterCategoryDiv}>L</div>
+                <div className={styles.filterCategoryDiv}>XL</div>
+              </div>
+            ) : null}
+          </div>
+
+          <div className={styles.firstFilterCat}>
+            <div className={styles.cardInfoDiv}>
+              <div className={styles.filterName}>Color</div>
+              <div className={styles.filterPlusSign}>+</div>
+            </div>
+          </div>
+          <button className={styles.filterButton}>APPLY</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Filter;
